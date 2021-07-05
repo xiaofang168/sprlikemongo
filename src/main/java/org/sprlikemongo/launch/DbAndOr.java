8145d74f25e63f3427bdd495d932c24f37c971c2
fangjie <fangjie@etouch.cn>
2021-07-05 15:04:28 +0800

    and or语句

--- src/main/java/org/sprlikemongo/launch/DbAndOr.java
+++ src/main/java/org/sprlikemongo/launch/DbAndOr.java
@@ -1,8 +1,36 @@
 package org.sprlikemongo.launch;
 
+import org.sprlikemongo.ql.DbQl;
+import org.sprlikemongo.ql.WhereQl;
+
 /**
  * @author fangjie
  * @date Created in 4:37 下午 2021/7/1.
  */
 public class DbAndOr {
+    /**
+     * 或者关系
+     *
+     * @param whereQls
+     * @return
+     */
+    public static AndOrOperator operator(WhereQl... whereQls) {
+        return new AndOrOperator(whereQls);
+    }
+
+    public static class AndOrOperator implements DbQl {
+
+        private WhereQl[] whereQls;
+
+        private AndOrOperator() {
+        }
+
+        private AndOrOperator(WhereQl... whereQls) {
+            this.whereQls = whereQls;
+        }
+
+        public WhereQl[] getWhereQls() {
+            return whereQls;
+        }
+    }
 }
